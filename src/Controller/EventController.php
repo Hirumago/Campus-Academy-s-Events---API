75442be116c6192d2da0c90b98d58cb28d3e4dda
Bastien <pma.Bastien.aubry@gmail.com>
2019-12-15 19:51:09 +0100

    purge event controler

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -22,29 +22,11 @@ class EventController extends BaseController
 
         $event = $this->getDoctrine()->getManager()->getRepository(Event::class)->findBy(array('idEvent' => 1));
         $data = $this->serialize($event);
-
-
-        //$data = "{
-          // \"Accept-Language\": \"en-US,en;q=0.8\",
-           //\"Host\": \"headers.jsontest.com\",
-           //\"Accept-Charset\": \"ISO-8859-1,utf-8;q=0.7,*;q=0.3\",
-           //\"Accept\": \"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\"
-           // }";
-
-
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
     }
 
-    /**
-     * @Route("/test", name="test")
-     */
-    public function testAction()
-    {
-        return $this->render('test.html.twig', [
-            'test' => 'test'
-        ]);
-    }
+
 }
