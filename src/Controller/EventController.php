510d0cf05b048b13f5ad4390b660687202c11031
Elevane <pma.bastien.aubry@gmail.com>
2020-01-26 20:52:43 +0100

    crud event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -4,49 +4,104 @@ namespace App\Controller;
 
 
 use App\Entity\Event;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/event")
+ */
 class EventController extends BaseController
 {
 
-
     /**
-     * @Route("/event/{id}", name="event_show", methods={"GET", "HEAD"})
-
-     * @param $id
-     * @return Response
+     * @Rest\View()
+     * @Get("/list")
      */
-    public function showAction($id)
+    public function listAction()
     {
 
-        $event = $this->getDoctrine()->getManager()->getRepository(Event::class)->findBy(array('idEvent' => $id));
-        $data = $this->serialize($event);
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
+        $users = $this->getDoctrine()->getRepository(Event::class)->findAll();
+        return $users;
     }
 
     /**
-     * @Route("/event", name="event_create" , methods={"POST"})
+     * @Route("/new", name="new", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function new(Request $request)
+    {
+
+        $data = $request->getContent();
+        $user = $this->deserialize($data, "App\Entity\Event");
+
+        if ($user) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return new JsonResponse(array("message" => "User crée", Response::HTTP_CREATED));
+        }
+        return new JsonResponse(array("message" => "deserialisation echoué"));
+    }
+
+    /**
+     * @Rest\View()
+     * @Get(
+     *     path = "/{id}",
+     *     name = "show",
+     *     requirements = {"id"="\d+"})
+     * @param string $id
+     * @return object|null
+     */
+    public function show($id)
+    {
+        $event = $this->getDoctrine()->getManager()->getRepository(Event::class)->findOneBy(array("idEvent" => $id));
+        return $event;
+    }
+
+
+    /**
+     * @Route("/edit/{id}", name="edit", methods={"PUT"})
+     * @param $id
+     * @param Request $request
+     * @return object|null
+     */
+    public function updateEvent(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $d = $request->getContent();
+        if (!empty($d)) {
+            $dbEvent = $em->getRepository(Event::class)->find($id);
+            $event = $this->deserialize($d, "App\Entity\Event");
+            $em->merge($event);
+            $em->flush();
+            return new JsonResponse(array("message" => "Event modifié"));
+        } else {
+            return new JsonResponse(array("message" => "requete invalide", Response::HTTP_NOT_MODIFIED));
+        }
+
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
+     * @Rest\Delete("/delete/{id}")
      * @param Request $request
      * @return Response
      */
-    public function createAction(Request $request)
+    public function delete($id, Request $request): Response
     {
-        $data = $request->getContent();
-        $event = $this->deserialize($data, 'App\Entity\Event');
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($event);
-        $em->flush();
+        $entityManager = $this->getDoctrine()->getManager();
+        $event = $entityManager->getRepository(Event::class)->find($id);;
+        if ($event) {
+            $entityManager->remove($event);
+            $entityManager->flush();
+            return new JsonResponse(array("messsage" => "event supprimé", Response::HTTP_OK));
+        }
 
-        return new Response('', Response::HTTP_CREATED);
+        return new JsonResponse("false");
     }
-
-
-
-
 }
